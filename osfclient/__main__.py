d4bfc7f0bf483c37ae4459ddaa67034884c7de03
Tim Head <betatim@gmail.com>
2017-04-13 15:50:00 +0200

    Move documentation from argument parser to function definitions

--- osfclient/__main__.py
+++ osfclient/__main__.py
@@ -12,20 +12,19 @@ def main():
     subparsers = parser.add_subparsers()
 
     # Clone project
-    clone_parser = subparsers.add_parser('clone',
-                                         description=('Copy all files from all'
-                                                      ' storages for project.')
-                                         )
+    clone_parser = subparsers.add_parser(
+        'clone', description=clone.__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
     clone_parser.set_defaults(func=clone)
     clone_parser.add_argument('project', help='OSF project ID')
     clone_parser.add_argument('output', help='Write files to this directory',
                               default=None, nargs='?')
 
     # Fetch an individual file
-    fetch_parser = subparsers.add_parser('fetch',
-                                         description=('Fetch an individual'
-                                                      ' file from a project.')
-                                         )
+    fetch_parser = subparsers.add_parser(
+        'fetch', description=fetch.__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter
+        )
     fetch_parser.set_defaults(func=fetch)
     fetch_parser.add_argument('-f', help='Force overwriting of local file',
                               action='store_true')
@@ -36,18 +35,18 @@ def main():
                               default=None, nargs='?')
 
     # List all files in a project
-    list_parser = subparsers.add_parser('list', aliases=['ls'],
-                                        description=('List all files from all'
-                                                     ' storages for project.')
-                                        )
+    list_parser = subparsers.add_parser(
+        'list', aliases=['ls'], description=list_.__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter
+        )
     list_parser.set_defaults(func=list_)
     list_parser.add_argument('project', help='OSF project ID')
 
     # Upload a single file
-    upload_parser = subparsers.add_parser('upload',
-                                          description=('Upload a new file to'
-                                                       ' an existing project.')
-                                          )
+    upload_parser = subparsers.add_parser(
+        'upload', description=upload.__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter
+        )
     upload_parser.set_defaults(func=upload)
     upload_parser.add_argument('project', help='OSF project ID')
     upload_parser.add_argument('source', help='Local file')
