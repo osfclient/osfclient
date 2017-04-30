eec959970cea97067bb5ebb2709e77fd78604b45
Tim Head <betatim@gmail.com>
2017-04-30 08:49:02 +0200

    Add tests for main entry point

---
+++ osfclient/tests/test_main.py
@@ -0,0 +1,29 @@
+import sys
+from unittest.mock import patch
+
+import pytest
+
+from osfclient.__main__ import main
+
+
+def test_no_args(capsys):
+    test_args = ['osf']
+    with patch.object(sys, 'argv', test_args):
+        main()
+
+    out, err = capsys.readouterr()
+    assert not out
+    assert not err
+
+
+@pytest.mark.parametrize("command",
+                         ['clone', 'fetch', 'list', 'upload', 'remove'])
+def test_command_prints_help(command, capsys):
+    test_args = ['osf', command]
+    with patch.object(sys, 'argv', test_args):
+        with pytest.raises(SystemExit):
+            main()
+
+    out, err = capsys.readouterr()
+    expected = 'usage: osf %s' % command
+    assert expected in err
