429a7ac52eed84ccb91d52f203e54b46e314158a
Bastian Greshake <bgreshake@googlemail.com>
2017-06-01 23:07:59 +0200

    first init version

--- osfclient/cli.py
+++ osfclient/cli.py
@@ -1,9 +1,20 @@
 """Command line interface to the OSF"""
-
+from __future__ import print_function
+from six.moves import input
 import os
 import getpass
 import sys
+<<<<<<< HEAD
 from six.moves import configparser
+=======
+
+
+
+try:
+    import configparser
+except ImportError:
+    import ConfigParser as configparser
+>>>>>>> first init version
 
 from tqdm import tqdm
 
@@ -65,6 +76,39 @@ def _setup_osf(args):
 
     return OSF(username=username, password=password)
 
+def init(args):
+    """Initialize or edit an existing .osfcli.config file
+
+    """
+    # reading existing config file, convert to configparser object
+    config = config_from_file()
+    config_ = configparser.ConfigParser()
+    config_.add_section('osf')
+    if 'username' not in config.keys():
+        config_.set('osf','username','')
+    else:
+        config_.set('osf','username',config['username'])
+    if 'project' not in config.keys():
+        config_.set('osf','project','')
+    else:
+        config_.set('osf','project',config['project'])
+
+    # now we can start asking for new values
+    print('Provide a username for the config file [current username: {}]:'.format(
+        config_.get('osf','username')))
+    username = input()
+    if username != '':
+        config_.set('osf','username',username)
+
+    print('Provide a project for the config file [current project: {}]:'.format(
+        config_.get('osf','project')))
+    project = input()
+    if project != '':
+        config_.set('osf','project',project)
+
+    cfgfile = open(".osfcli.config","w")
+    config_.write(cfgfile)
+    cfgfile.close()
 
 def clone(args):
     """Copy all files from all storages of a project.
