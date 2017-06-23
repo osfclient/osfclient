53d8efc342dece3195f08ab509b5dabbc18a2d7d
Tim Head <betatim@gmail.com>
2017-06-23 16:58:23 +0200

    Add missing dependency

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['requests', 'tqdm'],
+    install_requires=['requests', 'tqdm', 'six'],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
